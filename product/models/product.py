f16b3446359cf13edf7f7e442c6b138f883767dc
Danillo Guimarães <danillo.guimaraes.profissional@gmail.com>
2024-08-27 14:17:13 -0300

    Feat factories models for tests serializers, and tests serializers passed

--- product/models/product.py
+++ product/models/product.py
@@ -6,5 +6,5 @@ class Product(models.Model):
   title = models.CharField(max_length=100)
   description = models.TextField(max_length=500, blank=True, null=True)
   price = models.PositiveIntegerField(null=True)
-  acttive = models.BooleanField(default=True)
+  active = models.BooleanField(default=True)
   category = models.ManyToManyField(Category, blank=True)
